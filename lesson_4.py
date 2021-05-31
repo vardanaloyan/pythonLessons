24762238ff0cdc96c28918b8a5114176d46f0c24
Vardan <31190485+vardanaloyan@users.noreply.github.com>
2021-05-31 23:31:29 +0400

    added files via upload

---
+++ lesson_4.py
@@ -0,0 +1,58 @@
+"""
+Եթե ցանկանում եք աշխատացնել ծրագիրը uncomment արեք  (կոմենտից հանեք) այն հատվածը,
+որը ցանկանում եք աշխատացնել։
+Արագ կոմենտ անելու ձև՝
+Նշում ենք կոմենտ արվող հատվածը, սեղմում՝ ctrl + ?
+
+"""
+
+"""
+Մաս 1․ Մուտքային input ֆունկցիա
+"""
+#name = input("greq dzer anun@ ")
+#print(name)
+#print(type(name))
+
+"""
+Մաս 2․ x-ի y աստիճանը հաշվող ծրագիր 
+"""
+# x = int(input("x="))
+# y = int(input("y="))
+# z = x ** y
+# print("z=", z)
+
+"""
+Մաս 3․ Տողերի գումարում
+"""
+# q = "Yerevan"
+# c = "Armenia"
+# gumar = q + ", " + c
+# print(gumar)
+
+
+"""
+Մաս 4․ string-ի մեթոդներից մի քանիսի (upper, lower, capitalize,isdecimal) 
+կիրառության օրինակներ
+"""
+# print(q)
+# print(q.upper())
+# print(q.lower())
+# print(q.capitalize())
+# print(q.isdecimal())
+
+"""
+Մաս 5․ Պայմանի օպերատորներ` ==, <, >, <=, >=, !=, if-ի օգտագործումը
+"""
+# x = 8
+# if x == 7:
+#     print("Yes")
+# else:
+#     print("No")
+#
+
+"""
+Մաս 6․ for ցիկլ
+"""
+for i in range(10):
+    # print(i)
+    print("-")
